782f310e0006129bd9af68c718576ce797517c56
Kristoffergidd <72502045+Kristoffergidd@users.noreply.github.com>
2024-09-27 20:00:23 +0200

    finale

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,14 +1,4 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
 
-// export default defineConfig({
-//   plugins: [react()],
-//   test: {
-//     environment: 'jsdom', // Add this line
-//   },
-// });
-
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
